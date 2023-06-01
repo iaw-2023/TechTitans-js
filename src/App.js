8854209c2569fb3f7dd6e006996b5d133b38012a
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-06-01 14:48:53 -0300

    Listado de canchas se muestra en /canchas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Canchas from './Canchas.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
   
-    return (
-      <div>
-      <Canchas />
-    </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/canchas" element={<Canchas />} />
+      </Routes>
+    </Router>
+    
+  );
   };
   
 export default App;
