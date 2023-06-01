3532f44ebf781a3f872a3247cf4fa8ebb04c03ac
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-06-01 15:28:48 -0300

    Listado de turnos se muestra en /turnos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Canchas from './Canchas.js'
+import Turnos from './Turnos.js'
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/canchas" element={<Canchas />} />
+        <Route path="/turnos" element={<Turnos />} />
       </Routes>
     </Router>
     
---
+++ src/Turnos.js
@@ -0,0 +1,38 @@
+import {API} from './config.js';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const Turnos = () => {
+    const [turnos, setTurnos] = useState([]);
+    
+    useEffect(() => {
+      const fetchTurnos  = async () => {
+        try {
+          const response = await fetch(API+'/turnos');
+          const data = await response.json();
+          setTurnos(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchTurnos();
+    }, []);
+  
+    return (
+      <div>
+        <h2>Listado de Turnos</h2>
+        {turnos.map(turno =>
+        (
+          <div key={turno.id}>
+            <h3>ID Turno:{turno.id}</h3>
+            <p>ID cancha: {turno.id_cancha}</p>
+            <p>Fecha: {new Date(turno.fecha_turno).toLocaleDateString('es-ES')}</p>
+            <p>Hora: {turno.hora_turno}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+export default Turnos;
