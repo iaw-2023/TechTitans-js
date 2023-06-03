836892e1688392ae5a0ce401290d30c46dd829ba
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-06-03 20:51:37 -0300

    mis reservas se muestran con estilo, revisar doble precio

--- src/components/mis-reservas/Reservas.js
+++ src/components/mis-reservas/Reservas.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {API} from '../../config.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Reservas.css'
+
 
 const Reservas = () => {
   const [reservas, setReservas] = useState([]);
@@ -10,27 +13,38 @@ const Reservas = () => {
         const response = await fetch(API+'reservas/misReservas/raul@gmail.com');
         const data = await response.json();
         setReservas(data);
-        console.log(data)
+        console.log(data);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchReservas();
-    console.log(reservas);
   }, []);
 
   return (
-    <div>
-      <h2>Reservas de raul@gmail.com</h2>
-      {reservas.map(reserva => (
-        <div key={reserva.id}>
-          <h3>{reserva.reserva.id}</h3>
-          <p>Cliente: {reserva.reserva.email_cliente}</p>
-          <p>Fecha: {new Date(reserva.reserva.fecha_reserva).toLocaleDateString('es-ES')}</p>
-          <p>Hora: {reserva.reserva.hora_reserva}</p>
-        </div>
-      ))}
+    <div className="card-container">
+      <h2>Mis reservas son:</h2>
+      <div className="row">
+        {reservas.map((reserva, index) => (
+          <div className="col-md-4 mb-4" key={reserva.id}>
+            <div className="card border-primary mb-3 text-bg-dark mb-3">
+              <div className="card-body" key={index}>
+                <h3 className="card-title">{reserva.reserva.id}</h3>
+                <p className="card-text">Cliente: {reserva.reserva.email_cliente}</p>
+                <p className="card-text">Fecha: {new Date(reserva.reserva.fecha_reserva).toLocaleDateString('es-ES')}</p>
+                <p className="card-text">Hora: {reserva.reserva.hora_reserva}</p>
+                <p className="card-text">Precio: {reserva.detalles.map((detalle, detalleIndex) => (
+                  <span key={detalleIndex}>
+                    {detalle.precio}
+                    {detalleIndex !== reserva.detalles.length - 1 && ', '}
+                  </span>
+          ))}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
