e2267474ac9de897304f9e3edb19c5fc5ea887a2
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-06-04 20:14:20 -0300

    seccion contacto

--- src/components/contacto/Contacto.js
+++ src/components/contacto/Contacto.js
@@ -1,15 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import bosters from '../../components/imagenes/bosters.jpg';
 const Contacto = () => {
-    return(
-        <>
-        <h2>Contacto</h2>
-            <h3>WhatsApp</h3>
-                <p>291-123456</p>
-            <h3>Facebook</h3>
-                <p>/ReservaTuCancha</p>
-            <h3>Instagram</h3>
-                <p>@reservatucancha</p>
-        </>
-    );
-}
+  return (
+    <div className="text-center">
+  <h2>Contacto</h2>  
+  <div className="mb-3">
+    <a className="icon-link" href="https://api.whatsapp.com/" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faWhatsapp} className="bi" aria-hidden="true" />
+      2921231199
+    </a>
+  </div>
+
+  <div className="mb-3">
+    <a className="icon-link" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faInstagram} className="bi" aria-hidden="true" />
+      reservatucancha
+    </a>
+  </div>
+
+  <div className="mb-3">
+    <a className="icon-link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon={faFacebook} className="bi" aria-hidden="true" />
+      reservatucancha
+    </a>
+  </div>
+
+  <div className="mb-3">
+    <img src={bosters} alt="Imagen 3" width="600"  className="mb-3" />
+  </div>
+</div>
+
+  );
+};
 
 export default Contacto;
