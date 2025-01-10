53619ef4434121ebadc2d394579423b9a34ab3ad
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2025-01-10 18:21:12 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API = 'https://tech-titans-laravel-121212-hcgif35sv-danilos-projects-d24abcb2.vercel.app/rest/';
+export const API = 'https://tech-titans-laravel-121212.vercel.app/rest/';
