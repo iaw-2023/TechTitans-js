57565514e034d68898f24a156664e38cd3c725f5
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2025-05-06 11:41:33 -0300

    Update CarritoReservas.js

--- src/components/carrito/CarritoReservas.js
+++ src/components/carrito/CarritoReservas.js
@@ -135,10 +135,12 @@ const CarritoReservas = () => {
   }, [obtenerTurnos])
 
   const formatearFecha = (fechaStr) => {
-    const fecha = new Date(selectedTurno.fechaStr)
+    const fecha = new Date(fechaStr)
     fecha.setDate(fecha.getDate() + 1)
-    const [year, month, day] = fecha.split("-")
-    return `${day}/${month}/${year}`
+    const dia = String(fecha.getDate()).padStart(2, "0")
+  const mes = String(fecha.getMonth() + 1).padStart(2, "0")
+  const anio = fecha.getFullYear()
+  return `${dia}/${mes}/${anio}`
   }
 
   const renderTabla = () => {
